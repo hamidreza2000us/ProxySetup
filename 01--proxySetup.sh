9d10ce3466c278810ee361c461263de79c2724c6
hamidreza2000us <66959310+hamidreza2000us@users.noreply.github.com>
2020-09-26 01:33:49 +0330

    change

--- 01--proxySetup.sh
+++ 01--proxySetup.sh
@@ -6,11 +6,17 @@ bash ~/proxysetup/CO78-BaseProxyParameters.sh
 source ~/proxysetup/variables.sh
 ssh-copy-id root@$ProxyIP
 scp -r ~/proxysetup root@$ProxyIP:~/
+ssh $ProxyIP bash yum -y localinstall http://$ForemanHOSTNAME/pub/katello-ca-consumer-latest.noarch.rpm
+ssh $ProxyIP bash subscription-manager register --org "myorg"
 ssh $ProxyIP bash ~/proxysetup/CO78--Foreman-proxy-BaseSystem.sh
 ssh $ProxyIP bash ~/proxysetup/CO78-SetupChronyClient.sh 
 ssh $ProxyIP bash ~/proxysetup/CO78-IDMRegister.sh
 ssh $ProxyIP bash ~/proxysetup/CO78-ProxySetup.sh
-script=$(foreman-proxy-certs-generate --foreman-proxy-fqdn "$ProxyHOSTNAME" --certs-tar "/root/$ProxyHOSTNAME-certs.tar")
+scirpt=$(foreman-proxy-certs-generate --foreman-proxy-fqdn "$ProxyHOSTNAME" --certs-tar "/root/$ProxyHOSTNAME-certs.tar")
+echo $script | sed -n '/foreman-installer \\/,$p' | sed 's/\\//g' > /tmp/proxyscript.sh
+
 scp "/root/$ProxyHOSTNAME-certs.tar" root@$ProxyIP:~/
-ssh $ProxyIP bash $script
+scp /tmp/proxyscript.sh root@$ProxyIP:/tmp/proxyscript.sh
+ssh $ProxyIP bash /tmp/proxyscript.sh
+
 
--- CO78-ProxySetup.sh
+++ CO78-ProxySetup.sh
@@ -7,4 +7,8 @@ yum -y localinstall https://yum.puppet.com/puppet6-release-el-7.noarch.rpm
 yum -y localinstall https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 yum -y install foreman-release-scl
 yum install -y https://yum.theforeman.org/client/latest/el7/x86_64/foreman-client-release-2.1.1-1.el7.noarch.rpm
+
 yum install -y foreman-proxy-content
+
+if  [  $( firewall-cmd --query-service=RH-Satellite-6) == 'no'  ] ; then firewall-cmd --permanent --add-service=RH-Satellite-6 ; fi
+firewall-cmd --reload
