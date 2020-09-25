969dddf02a1fa85673f66455f092de921b186662
hamidreza2000us <66959310+hamidreza2000us@users.noreply.github.com>
2020-09-26 00:28:26 +0330

    change

--- variables.sh
+++ variables.sh
@@ -1,14 +1,11 @@
 export IDMHOSTNAME=idm.myhost.com
 export IDMIP=192.168.13.11
-export IDMNETMASK=24
-export IDMGW=192.168.13.2
 export IDMDomain=myhost.com
-export IDMPass=Iahoora@123                
 export IDMRealm=MYHOST.COM
-
-export ForemanHOSTNAME=foreman.myhost.com
+export IDMPass=Iahoora@123
+export ProxyHOSTNAME=proxy01.myhost.com
+export ProxyIP=192.168.13.51
+export ProxyNETMASK=24
+export ProxyGW=192.168.13.2
 export ForemanIP=192.168.13.250
-export ForemanNETMASK=24
-export ForemanGW=192.168.13.2
-export ForemanDNSSERVER=8.8.8.8
-export ForemanPass=Iahoora@123
+export ForemanHOSTNAME=foreman.myhost.com
