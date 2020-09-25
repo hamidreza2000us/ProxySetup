79b81ac8ce44d4e0c559e742e3112bf6bd77d864
hamidreza2000us <66959310+hamidreza2000us@users.noreply.github.com>
2020-09-26 00:37:35 +0330

    change

--- CO78-BaseProxyParameters.sh
+++ CO78-BaseProxyParameters.sh
@@ -5,18 +5,17 @@ then
   source ~/proxysetup/variables.sh
 fi
 
+
 read -rp "IDM Hostname to use: ($IDMHOSTNAME): " choice; [[ -n "${choice}"  ]] &&  export IDMHOSTNAME="$choice"; 
 read -rp "IDM IP to use: ($IDMIP): " choice; [[ -n "${choice}"  ]] &&  export IDMIP="$choice"; 
 read -rp "IDM domain to use: ($IDMDomain): " choice; [[ -n "${choice}"  ]] &&  export IDMDomain="$choice";
 read -rp "IDM Realm to use: ($IDMRealm): " choice; [[ -n "${choice}"  ]] &&  export IDMRealm="$choice";
 read -rp "IDM Password to use: ($IDMPass): " choice; [[ -n "${choice}"  ]] &&  export IDMPass="$choice";
 
-read -rp "Foreman Hostname to use: ($ProxyHOSTNAME): " choice;[[ -n "${choice}"  ]] &&  export ProxyHOSTNAME="$choice";
-read -rp "Foreman IP to use: ($ProxyIP): " choice; [[ -n "${choice}"  ]] &&  export ProxyIP="$choice";
-read -rp "Foreman Netmask to use (just the number of bits): ($ProxyNETMASK): " choice;	[[ -n "${choice}"  ]] &&  export ProxyNETMASK="$choice";
-read -rp "Foreman GW to use: ($ProxyGW): " choice; [[ -n "${choice}"  ]] &&  export ProxyGW="$choice";
-read -rp "Foreman Global DNS to use: ($ProxyDNSSERVER): " choice;	[[ -n "${choice}"  ]] &&  export ProxyDNSSERVER="$choice";
-read -rp "Foreman Password to use: ($ProxyPass): " choice;	[[ -n "${choice}"  ]] &&  export ForemanPass="$choice";
+read -rp "Proxy Hostname to use: ($ProxyHOSTNAME): " choice;[[ -n "${choice}"  ]] &&  export ProxyHOSTNAME="$choice";
+read -rp "Proxy IP to use: ($ProxyIP): " choice; [[ -n "${choice}"  ]] &&  export ProxyIP="$choice";
+read -rp "Proxy Netmask to use (just the number of bits): ($ProxyNETMASK): " choice;	[[ -n "${choice}"  ]] &&  export ProxyNETMASK="$choice";
+read -rp "Proxy GW to use: ($ProxyGW): " choice; [[ -n "${choice}"  ]] &&  export ProxyGW="$choice";
 
 echo "export IDMHOSTNAME=$IDMHOSTNAME" > ~/proxysetup/variables.sh
 echo "export IDMIP=$IDMIP" >> ~/proxysetup/variables.sh
@@ -27,8 +26,6 @@ echo "export ProxyHOSTNAME=$ProxyHOSTNAME" >> ~/proxysetup/variables.sh
 echo "export ProxyIP=$ProxyIP" >> ~/proxysetup/variables.sh
 echo "export ProxyNETMASK=$ProxyNETMASK" >> ~/proxysetup/variables.sh
 echo "export ProxyGW=$ProxyGW" >> ~/proxysetup/variables.sh
-echo "export ProxyDNSSERVER=$ProxyDNSSERVER" >> ~/proxysetup/variables.sh
-echo "export ForemanPass=$ForemanPass" >> ~/proxysetup/variables.sh
 
 ForemanIP=${IP:="$(ip route get 8.8.8.8 | awk '{print $7; exit}')"}
 echo "export ForemanIP=$ForemanIP" >> ~/proxysetup/variables.sh
